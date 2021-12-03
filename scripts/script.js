e93a9f21ee2138e1d6368ce7abd5d446342ac3df
Ivan Popov <ivankireley@yandex.ru>
2021-12-03 23:08:47 +0300

    #update.1 исправления ревьюера

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,25 @@
-let formElement = document.querySelector(".popup__form");
-let nameInput = formElement.querySelector(".popup__input_type_name");
-let jobInput = formElement.querySelector(".popup__input_type_occupation");
-let closeButton = document.querySelector(".popup__close-button");
-let profileName = document.querySelector(".profile__name-title");
-let profileJob = document.querySelector(".profile__occupation");
-let editButton = document.querySelector(".profile__name-button");
-let popup = document.querySelector(".popup");
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_occupation');
+let closeButton = document.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name-title');
+let profileJob = document.querySelector('.profile__occupation');
+let editButton = document.querySelector('.profile__name-button');
+let popup = document.querySelector('.popup');
 
+/* закрытие попапа */
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
+/* открытие попапа */
 function openPopup() {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
+/* сохранение попапа */
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -24,6 +27,7 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
+/* эвенты */
 closeButton.addEventListener('click', closePopup);
 editButton.addEventListener('click', openPopup);
 formElement.addEventListener('submit', formSubmitHandler);
